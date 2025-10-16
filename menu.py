0b9c87671fd7e2a59e54ff6218d5adb8b2947932
Rodrigo <rodrihercar24@gmail.com>
2025-10-16 09:18:29 -0600

    menu

---
+++ menu.py
@@ -0,0 +1,18 @@
+
+while True:
+    print("Menu de metodos de ordenamiento\n"
+          "1. Carrera de buses (Quicksort vs sort)\n"
+          "2. Rifa (Bogo)\n"
+          "3. Ordenar edades \n"
+          "4. Salir")
+    select = (input("selecciona una opcion: "))
+    match select:
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4":
+            print("Saliendo del sistema")
+            break
